4a5eb435d07851bbecbf7b1f95cc257df377d7e2
R. Nick Vandemark <rnvandemark@gmail.com>
2023-03-26 19:59:02 -0400

    tmp

--- ron_description/launch/robot_node_bringup.launch.py
+++ ron_description/launch/robot_node_bringup.launch.py
@@ -1,118 +1,362 @@
+#from launch import LaunchDescription
+#from launch.actions import DeclareLaunchArgument, EmitEvent, GroupAction, LogInfo, RegisterEventHandler
+#from launch.event_handlers import OnProcessExit
+#from launch.events import Shutdown
+#from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+#from launch_ros.actions import Node, PushRosNamespace
+#from launch_ros.substitutions import FindPackageShare
+#
+#def generate_launch_description():
+#    launch_desc = LaunchDescription()
+#
+#    #
+#    # Declare the launch arguments
+#    #
+#
+#    namespace = LaunchConfiguration("namespace")
+#    declare_namespace_cmd = DeclareLaunchArgument(
+#        "namespace",
+#        default_value="",
+#        description="Top-level namespace"
+#    )
+#    launch_desc.add_action(declare_namespace_cmd)
+#
+#    robot_name = LaunchConfiguration("robot_name")
+#    launch_desc.add_action(DeclareLaunchArgument(
+#        "robot_name",
+#        default_value="",
+#        description="Unique robot name"
+#    ))
+#
+#    pose = {}
+#    for cart_dimension in ["x", "y", "z", "R", "P", "Y"]:
+#        pose[cart_dimension] = LaunchConfiguration(cart_dimension)
+#        launch_desc.add_action(DeclareLaunchArgument(
+#            cart_dimension,
+#            default_value="0.0",
+#            description="Initial {0} value".format(cart_dimension)
+#        ))
+#
+#    controller_manager_name = LaunchConfiguration(
+#        "controller_manager_name",
+#        default=[robot_name, "_controller_manager"]
+#    )
+#
+#    #
+#    # Get URDF via xacro
+#    #
+#
+#    robot_description={
+#        "robot_description": Command([
+#            PathJoinSubstitution([FindExecutable(name="xacro")]),
+#            " ",
+#            PathJoinSubstitution([FindPackageShare("diffbot_description"), "urdf", "diffbot.urdf.xacro"]),
+#        ])
+#    }
+#
+#    launch_desc.add_action(
+#        Node(
+#            package="controller_manager",
+#            executable="ros2_control_node",
+#            name=controller_manager_name,
+#            parameters=[
+#                robot_description,
+#                PathJoinSubstitution([FindPackageShare("ron_description"), "config", "robot_node_controller_manager.yaml"]),
+#            ],
+#            output="both",
+#        )
+#    )
+#
+##    joint_state_broadcaster_spawner = Node(
+##        package="controller_manager",
+##        executable="spawner",
+##        arguments=[
+##            "joint_state_broadcaster",
+##            "-c",
+##            controller_manager_name,
+##        ],
+##    )
+#
+#    rviz_node = Node(
+#        package="rviz2",
+#        executable="rviz2",
+#        name="rviz2",
+#        arguments=["-d", PathJoinSubstitution([FindPackageShare("diffbot_description"), "config", "diffbot.rviz"])],
+#        output="screen",
+#    )
+#
+#    launch_desc.add_action(
+#        GroupAction([
+#            PushRosNamespace(
+#                namespace=namespace,
+#            ),
+#            Node(
+#                package="robot_state_publisher",
+#                executable="robot_state_publisher",
+#                parameters=[robot_description],
+#                remappings=[
+##                    ("diff_drive_controller/cmd_vel_unstamped", "cmd_vel"),
+#                ],
+#                output="both",
+#            ),
+#            Node(
+#                package="gazebo_ros",
+#                executable="spawn_entity.py",
+#                arguments=[
+#                    "-entity", robot_name,
+#                    "-topic", "robot_description",
+#                    "-robot_namespace", namespace,
+#                    "-x", pose["x"],
+#                    "-y", pose["y"],
+#                    "-z", pose["z"],
+#                    "-R", pose["R"],
+#                    "-P", pose["P"],
+#                    "-Y", pose["Y"],
+#                ],
+#                output="screen",
+#            ),
+##            joint_state_broadcaster_spawner,
+##            # Delay rviz start after `joint_state_broadcaster`
+##            RegisterEventHandler(
+##                event_handler=OnProcessExit(
+##                    target_action=joint_state_broadcaster_spawner,
+##                    on_exit=[rviz_node],
+##                )
+##            ),
+#            rviz_node,
+#            RegisterEventHandler(
+#                event_handler=OnProcessExit(
+#                    target_action=rviz_node,
+#                    on_exit=EmitEvent(event=Shutdown(reason="rviz exited"))
+#                )
+#            ),
+##            # Delay load of diff drive controller after `joint_state_broadcaster`
+##            RegisterEventHandler(
+##                event_handler=OnProcessExit(
+##                    target_action=joint_state_broadcaster_spawner,
+##                    on_exit=[
+##                        Node(
+##                            package="controller_manager",
+##                            executable="spawner",
+##                            namespace=namespace,
+##                            arguments=[
+##                                "diff_drive_controller",
+##                                "--load-only",
+##                                "-c",
+##                                controller_manager_name,
+##                            ],
+##                        )
+##                    ],
+##                )
+##            )
+#        ])
+#    )
+#
+#    return launch_desc
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, LogInfo, RegisterEventHandler
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+import xacro
 
 def generate_launch_description():
-    ROBOT_NODE_PARAM_NAME = "robot_node"
-    CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME = "controller_manager_node_name"
-    JOINT_STATE_BROADCASTER_NODE_NAME_PARAM_NAME = "joint_state_broadcaster_node_name"
-    DIFF_DRIVE_CONTROLLER_NODE_NAME_PARAM_NAME = "diff_drive_controller_node_name"
+    #
+    # Declare launch arguments
+    #
 
-    ROBOT_NODE_NAME_PARAM_NAME = "robot_node_name"
-
-    declared_node_id = DeclareLaunchArgument(
-        name=NODE_ID_PARAM_NAME,
-        default_value="0",
-        description="ID of this RON robot node",
+    namespace = LaunchConfiguration("namespace")
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        default_value="",
+        description="Top-level namespace"
     )
-    node_id = LaunchConfiguration(NODE_ID_PARAM_NAME)
 
-    declared_controller_manager_node_name = DeclareLaunchArgument(
-        name=CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME,
+    robot_name = LaunchConfiguration("robot_name")
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        "robot_name",
+        default_value="",
+        description="Unique robot name"
+    )
+
+    controller_manager_name = LaunchConfiguration("controller_manager_name")
+    declare_controller_manager_name_cmd = DeclareLaunchArgument(
+        "controller_manager_name",
         default_value="controller_manager",
-        description="The relative name of the RON system controller manager node",
-    )
-    controller_manager_node_name = LaunchConfiguration(CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME)
-
-    declared_joint_state_broadcaster_node_name = DeclareLaunchArgument(
-        name=JOINT_STATE_BROADCASTER_NODE_NAME_PARAM_NAME,
-        default_value="joint_state_broadcaster",
-        description="The relative name of this robot's joint state broadcaster node",
-    )
-    joint_state_broadcaster_node_name = LaunchConfiguration(JOINT_STATE_BROADCASTER_NODE_NAME_PARAM_NAME)
-
-    declared_diff_drive_controller_node_name = DeclareLaunchArgument(
-        name=DIFF_DRIVE_CONTROLLER_NODE_NAME_PARAM_NAME,
-        default_value="diff_drive_controller",
-        description="The relative name of this robot's diff drive controller node",
-    )
-    diff_drive_controller_node_name = LaunchConfiguration(DIFF_DRIVE_CONTROLLER_NODE_NAME_PARAM_NAME)
-
-    robot_node_name = "robot_node_" + node_id
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            robot_node_name + "/" + joint_state_broadcaster_node_name,
-            "-c",
-            controller_manager_node_name
-        ],
+        description="Name of this robot's controller manager node"
     )
 
+    use_rviz = LaunchConfiguration("use_rviz")
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        "use_rviz",
+        default_value="true",
+        description="Whether or not to launch RViz"
+    )
+
+    pose = {}
+    declare_pose_cmds = {}
+    for cart_dimension in ["x", "y", "z", "R", "P", "Y"]:
+        pose[cart_dimension] = LaunchConfiguration(cart_dimension)
+        declare_pose_cmds[cart_dimension] = DeclareLaunchArgument(
+            cart_dimension,
+            default_value="0.0",
+            description="Initial {0} value".format(cart_dimension)
+        )
+
+    absolute_controller_manager_name = LaunchConfiguration(
+        "absolute_controller_manager_name",
+        default=["/", namespace, "/", controller_manager_name]
+    )
+
+    #
+    # Get URDF via xacro
+    #
+
+    robot_description = {
+        "robot_description": Command([
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ron_description"), "urdf", "robot_node.urdf.xacro"]),
+            " robot_node_name:=",
+            robot_name
+        ])
+    }
+
+#    control_node = Node(
+#        package="controller_manager",
+#        executable="ros2_control_node",
+#        namespace=namespace,
+#        parameters=[
+#            robot_description,
+#            PathJoinSubstitution([FindPackageShare("ron_description"), "config", "robot_node_controller_manager.yaml"])
+#        ],
+#        output="both",
+#    )
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        namespace=namespace,
         output="both",
-        parameters=[{"robot_description": Command([
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("ron_description"), "urdf", "robot_node.urdf.xacro"]
-            ),
-            " ",
-            ROBOT_NODE_NAME_PARAM_NAME,
-            ":=",
-            robot_node_name,
-        ])}],
-    )
-
-    robot_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            robot_node_name + "/" + diff_drive_controller_node_name,
-            "-c",
-            controller_manager_node_name
+        parameters=[
+            robot_description,
+            {"frame_prefix": robot_name}
         ],
-    )
-
-    # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[robot_controller_spawner],
-        )
-    )
-
-    # Delay rviz start after `joint_state_broadcaster`
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("ron_description"), "config", "example.rviz"]
+#        remappings=[
+#            ("/tf", "tf"),
+#            ("/tf_static", "tf_static"),
+#        ],
     )
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
+        namespace=namespace,
         output="log",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", PathJoinSubstitution([FindPackageShare("ron_description"), "config", "example.rviz"])],
+        condition=IfCondition(use_rviz),
     )
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "-c", absolute_controller_manager_name],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "-c", absolute_controller_manager_name],
+    )
+
+    log_entity_pose = LogInfo(msg=[
+        "'", robot_name, "' entity spawn at [x=", pose["x"], ", y=", pose["y"], ", z=", pose["z"],
+        ", R=", pose["R"], ", P=", pose["P"], ", Y=", pose["Y"], "]"
+    ])
+
+    entity_spawner = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        namespace=namespace,
+        arguments=[
+            "-entity", robot_name,
+            "-topic", "robot_description",
+            "-robot_namespace", namespace,
+            "-x", pose["x"],
+            "-y", pose["y"],
+            "-z", pose["z"],
+            "-R", pose["R"],
+            "-P", pose["P"],
+            "-Y", pose["Y"],
+        ],
+        output="screen",
+    )
+
+    delay_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=entity_spawner,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
+    delay_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=entity_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    delay_rviz = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=joint_state_broadcaster_spawner,
             on_exit=[rviz_node],
         )
     )
 
-    return LaunchDescription([
-        declared_node_id,
-        declared_controller_manager_node_name,
-        declared_joint_state_broadcaster_node_name,
-        declared_diff_drive_controller_node_name,
-        joint_state_broadcaster_spawner,
+    nodes = [
+        declare_namespace_cmd,
+        declare_robot_name_cmd,
+        declare_controller_manager_name_cmd,
+        declare_use_rviz_cmd,
+        declare_pose_cmds["x"],
+        declare_pose_cmds["y"],
+        declare_pose_cmds["z"],
+        declare_pose_cmds["R"],
+        declare_pose_cmds["P"],
+        declare_pose_cmds["Y"],
+        log_entity_pose,
+        delay_joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner,
+        delay_rviz,
+#        control_node,
         robot_state_pub_node,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
-    ])
+        entity_spawner,
+    ]
+
+    return LaunchDescription(nodes)
--- ron_description/launch/sim.launch.py
+++ ron_description/launch/sim.launch.py
@@ -1,116 +1,107 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, GroupAction, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    NAMESPACE = "/"
+#    NAMESPACE = "/"
+#
+#    NUM_ROBOT_NODES_PARAM_NAME = "num_robot_nodes"
+#    CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME = "controller_manager_node_name"
+#
+#    declared_num_robot_nodes = DeclareLaunchArgument(
+#        name=NUM_ROBOT_NODES_PARAM_NAME,
+#        default_value="0",
+#        description="Number of robot nodes in RON",
+#    )
+#    num_robot_nodes = LaunchConfiguration(NUM_ROBOT_NODES_PARAM_NAME)
+    num_robot_nodes = 1
+#
+#    declared_controller_manager_node_name = DeclareLaunchArgument(
+#        name=CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME,
+#        default_value="controller_manager",
+#        description="The relative name of the RON system controller manager node",
+#    )
+#    controller_manager_node_name = LaunchConfiguration(CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME)
+#
+#    robot_controllers = PathJoinSubstitution([
+#        FindPackageShare("ron_description"),
+#        "config",
+#        "diff_drive_controllers.yaml",
+#    ])
+#    control_node = Node(
+#        package="controller_manager",
+#        executable="ros2_control_node",
+#        parameters=[robot_description, robot_controllers],
+#        output="both",
+#    )
+#
+#    joint_state_broadcaster_spawner = Node(
+#        package="controller_manager",
+#        executable="spawner",
+#        arguments=["joint_state_broadcaster", "-c", CONTROLLER_MANAGER_NODE_NAME],
+#        namespace=NAMESPACE,
+#    )
 
-    NUM_ROBOT_NODES_PARAM_NAME = "num_robot_nodes"
-    CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME = "controller_manager_node_name"
-
-    declared_num_robot_nodes = DeclareLaunchArgument(
-        name=NUM_ROBOT_NODES_PARAM_NAME,
-        default_value="0",
-        description="Number of robot nodes in RON",
-    )
-    num_robot_nodes = LaunchConfiguration(NUM_ROBOT_NODES_PARAM_NAME)
-
-    declared_controller_manager_node_name = DeclareLaunchArgument(
-        name=CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME,
-        default_value="controller_manager",
-        description="The relative name of the RON system controller manager node",
-    )
-    controller_manager_node_name = LaunchConfiguration(CONTROLLER_MANAGER_NODE_NAME_PARAM_NAME)
-
-    robot_controllers = PathJoinSubstitution([
-        FindPackageShare("ron_description"),
-        "config",
-        "diff_drive_controllers.yaml",
-    ])
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-        namespace=NAMESPACE,
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "-c", CONTROLLER_MANAGER_NODE_NAME],
-        namespace=NAMESPACE,
-    )
-
-    launch_desc_actions = [
-        declared_num_robot_nodes,
-        control_node,
-        joint_state_broadcaster_spawner,
-    ]
-    for i in range(num_robot_nodes):
-        robot_description = {"robot_description": Command([
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("ron_description"), "urdf", "robot_node.urdf.xacro"]
-            ),
-            " ",
-            NODE_ID_PARAM_NAME,
-            ":=",
-            i,
-        ])}
-        robot_state_pub_node = Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            output="both",
-            parameters=[robot_description],
-            remappings=[
-                (NAMESPACE + "/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
-            ],
-            namespace=NAMESPACE,
-        )
-        launch_desc_actions.append(robot_state_pub_node)
-
-        robot_controller_spawner = Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["diff_drive_controller", "-c", NAMESPACE + CONTROLLER_MANAGER_NODE_NAME],
-            namespace=NAMESPACE,
-        )
-
-        # Delay start of robot_controller after `joint_state_broadcaster`
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[robot_controller_spawner],
+    launch_desc = LaunchDescription()
+    launch_desc.add_action(
+        GroupAction([
+            PushRosNamespace(namespace="/robot_node_0"),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution([
+                        FindPackageShare("gazebo_ros2_control_demos"),
+                        "launch",
+                        "diff_drive.launch.py"
+                    ])
+                ),
             )
-        )
-        launch_desc_actions.append(delay_robot_controller_spawner_after_joint_state_broadcaster_spawner)
+        ])
+    )
 
-    # Delay rviz start after `joint_state_broadcaster`
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("ron_description"), "config", "example.rviz"]
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        namespace=NAMESPACE,
-    )
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
-    launch_desc_actions.append(delay_rviz_after_joint_state_broadcaster_spawner)
+#    use_rviz = LaunchConfiguration("use_rviz")
+#    launch_desc.add_action(
+#        DeclareLaunchArgument(
+#            "use_rviz",
+#            default_value="true",
+#            description="Whether or not to launch RViz"
+#        )
+#    )
+#
+#    launch_desc.add_action(
+#        IncludeLaunchDescription(
+#            PythonLaunchDescriptionSource(
+#                PathJoinSubstitution([
+#                    FindPackageShare("gazebo_ros"),
+#                    "launch",
+#                    "gazebo.launch.py"
+#                ])
+#            ),
+#        )
+#    )
+#
+#    for i in range(num_robot_nodes):
+#        robot_name = "robot_node_{0}".format(i)
+#        launch_desc.add_action(
+#            IncludeLaunchDescription(
+#                PythonLaunchDescriptionSource(
+#                    PathJoinSubstitution([
+#                        FindPackageShare("ron_description"),
+#                        "launch",
+#                        "robot_node_bringup.launch.py"
+#                    ])
+#                ),
+#                launch_arguments={
+#                    "namespace": robot_name,
+#                    "robot_name": robot_name,
+#                    "use_rviz": use_rviz,
+#                    "x": str(i),
+#                    "z": "1.0",
+#                }.items()
+#            )
+#        )
 
-    return LaunchDescription(launch_desc_actions)
+    return launch_desc
